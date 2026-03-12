2d21eba318024d32f77a267494551144d7b690d1
yuvrajsingh59yt-collab <yuvrajsingh59yt@gmail.com>
2026-03-12 08:20:23 +0530

    Add files via upload

---
+++ ai.js
@@ -0,0 +1,49 @@
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+
+    const API_KEY = process.env.GEMINI_KEY;
+    const prompt = req.body.prompt;
+
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              role: "user",
+              parts: [{ text: prompt }]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    let reply = "Zenith AI error.";
+
+    if (data.candidates && data.candidates.length > 0) {
+      reply = data.candidates[0].content.parts[0].text;
+    }
+
+    res.status(200).json({ reply });
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: "Server error",
+      details: error.toString()
+    });
+
+  }
+
+}
